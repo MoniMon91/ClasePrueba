d21088722823e558b2e63928cb3359b6740cded4
Mónica Cuapio <monipelussa@gmail.com>
2025-07-25 20:03:07 -0600

    Add files via upload
    Ejercicio de operadores

---
+++ operadores.py
@@ -0,0 +1,38 @@
+# Operadores aritmeticos
+# print (2+2) #suma
+# print (4-2) #resta
+# print (3 *7) #multiplicación
+# print (15 / 2) #división
+# print (11 % 4) # (módulo) se lee once módulo 4 
+# print (11 // 4) # división de piso
+# print (2 ** 3) # potenciación
+
+# Operadores en cadenas
+# print ("Hola" + "mundo") #concatenación
+# print ("Hola" * 3) #repetición
+# print ("Hola" + "mundo" *3) #concatanación y repetición
+
+# Operadores de comparación
+# print ("Hola" == "hola") #Igual que
+# print ("Hola" != "hola") #distinto que
+# print (3 > 11) #mayor que,  menor que
+# print (11 >= 11) #mayor o igual que
+# print (10 <= 11) #menor o igual que
+
+# Operadores de existencia
+# print ("ola" in "Hola") #De existencia
+# print ("ola" not in "Hola") #De inexistencia
+
+# Operadores boleanos
+# print (True or False) #or verifica que se cumpla una de las condiciones
+# print (True and False) #and verifica que se cumplan las dos condiciones
+
+# Operadores de asignación
+#  x = 1 #Operador de asignación estándar
+#  x += 2 #Operados de asignación suma
+#  x*=3 #Operador de asignación multiplicación
+#  x %=4 #Operador de asignación módulo
+#  print (x)
+
+# x = 6 * 5 + 8 / 2 ** 4
+# print (x)
