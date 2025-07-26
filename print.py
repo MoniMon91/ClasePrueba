ca8e9399a7dfea446567cfb0785641faa5e8afed
Mónica Cuapio <monipelussa@gmail.com>
2025-07-25 20:24:18 -0600

    Add files via upload
    Usos de print

---
+++ print.py
@@ -0,0 +1,21 @@
+#Ejemplos de la función print ()
+
+# print ("Hola mundo")
+# print ("Hola mundo", "otra vez")
+# print ("Son las:", "09:00", "de la mañana")
+# print ("El resultado de 3*4 es:", 3*4)
+
+#Ejemplos de cadenas formateadas
+# print ("El número 15 en sistema decimal es el %d, en sistema octal es %o, en el sistema hexadecimal es %x" %(15, 15, 15))
+
+#pi = 3.141592
+#r=5
+#print (f"El radio de un círculo es {r} y el área de ese círculo es {pi * r ** 2: .2f}")
+
+#Impresión de caracteres especiales
+#print ("La letra beta es: \n\t \u03b2")
+
+#caracteres de escape
+print ("Hola mundo", end = " ")
+print ("otra vez", end = "\t") #La \t" es de tabulación
+print ("y otra vez")
